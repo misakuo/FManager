df5e050e6884b645f0b0edd114f6d238e692cab7
sodaless <sodaless@sodaless.net>
2014-06-19 19:55:20 +0800

    语言-英文类

---
+++ src/net/sodaless/fmanager/util/LanguageEN_US.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package net.sodaless.fmanager.util;
+
+/**
+ * @author Misaku
+ *
+ */
+public class LanguageEN_US extends Language{
+
+	public LanguageEN_US()
+	{
+		setLanguageType("EN-US");
+		VTSrefresh = "Refresh";
+		initCheckBoxText = "ÖÐÎÄ";
+		initFrameTitle = "Please enter the controller IP address";
+		initLableText = "Please enter the controller IP address and the REST service port";
+		initOnlineButtonText = "Launch the online mode";
+		initOfflineButtonText = "Launch the offline mode";
+		VTFrameTitle = "Virtual Topology Builder";
+		VTaddHost = "Add Host";
+		VTaddSwitch = "Add Switch";
+		VTaddLink = "Add Link";
+		VTfinish = "Complete";
+		VTfile = "Load form file";
+		VTsave = "Save to file";
+		VTclear = "Clear";
+		VTremove = "Remove element";
+		MWTitle = "FManager";
+		MWpm = "Path Manager";
+		MWfm = "Flow Manager";
+		MWfw = "FireWall";
+		MWlb = "Load Balance";
+		MWqos = "QoS";
+		PMTitle = "Path Manager";
+		PMloadalog = "Loading Algorithm";
+		PMcalc = "Path Calculation";
+		PMpush = "Push Path";
+		PMautopush = "Auto Push";
+		PMsymmetricpath = "Push Symmetrical Path";
+		PMsrc = "Routing SRC";
+		PMdst = "Routing DST";
+		PMbw = "Bandwidth Requirement";
+		PMcommit = "Commit";
+		PMadd = "Add Flow";
+		PMreset = "Reset";
+	}
+	
+}
+
