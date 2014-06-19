e96a1d387c9b42e7259f1cb8a5c93aaf3e233b72
sodaless <sodaless@sodaless.net>
2014-06-19 19:55:30 +0800

    语言-中文类

---
+++ src/net/sodaless/fmanager/util/LanguageZH_CN.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package net.sodaless.fmanager.util;
+
+/**
+ * @author Misaku
+ *
+ */
+public final class LanguageZH_CN extends Language{
+	
+	public LanguageZH_CN()
+	{
+		setLanguageType("ZH-CN");
+		VTSrefresh = "刷新拓扑";
+		initCheckBoxText = "English";
+		initFrameTitle = "请输入控制器IP地址";
+		initLableText = "请输入控制器IP地址及REST服务端口";
+		initOnlineButtonText = "启动在线模式";
+		initOfflineButtonText = "启动离线模式";
+		VTFrameTitle = "虚拟拓扑生成";
+		VTaddHost = "添加Host";
+		VTaddSwitch = "添加Switch";
+		VTaddLink = "添加Link";
+		VTfinish = "完成";
+		VTfile = "从文件读入";
+		VTsave = "保存到文件";
+		VTclear = "清空";
+		VTremove = "移除元素";
+		MWTitle = "FManager";
+		MWpm = "路径管理";
+		MWfm = "流表管理";
+		MWfw = "防火墙";
+		MWlb = "负载均衡";
+		MWqos = "QoS";
+		PMTitle = "路径管理";
+		PMloadalog = "加载算法";
+		PMcalc = "计算路径";
+		PMpush = "下发路径";
+		PMautopush = "计算完成后自动下发";
+		PMsymmetricpath = "下发对称路径";
+		PMsrc = "路由源结点";
+		PMdst = "路由目的结点";
+		PMbw = "带宽需求";
+		PMcommit = "提交流信息";
+		PMadd = "添加流信息";
+		PMreset = "重置";
+	}
+
+}
