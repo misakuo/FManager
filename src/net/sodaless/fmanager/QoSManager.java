4b80e753d8b4dc0d0e657f71fc5906d5733a3222
sodaless <sodaless@sodaless.net>
2014-06-28 10:16:25 +0800

    QoS策略管理模块

---
+++ src/net/sodaless/fmanager/QoSManager.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package net.sodaless.fmanager;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+/**
+ * @author Misaku
+ *
+ */
+public class QoSManager extends JFrame implements ActionListener {
+
+	/* （非 Javadoc）
+	 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+	 */
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO 自动生成的方法存根
+
+	}
+
+}
